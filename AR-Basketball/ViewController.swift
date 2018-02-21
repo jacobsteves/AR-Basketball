8d29bcd3e06532085095652ed975fed2e4b71d4a
Jacob Steves <jgs1998@hotmail.com>
2018-02-20 21:08:10 -0500

    initialize court

--- AR-Basketball/ViewController.swift
+++ AR-Basketball/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var planeDetected: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
+        let tapGestureRecongizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        self.sceneView.addGestureRecognizer(tapGestureRecongizer)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: [.existingPlaneUsingExtent])
+        if !hitTestResult.isEmpty {
+            self.addBasket(hitTestResult: hitTestResult.first!)
+        }
+    }
+    
+    func addBasket(hitTestResult: ARHitTestResult) {
+        let basketScene = SCNScene(named: "Basketball.scnassets/Basketball.scn")
+        let basketNode = basketScene?.rootNode.childNode(withName: "Basket", recursively: false)
+        let positionOfPlane = hitTestResult.worldTransform.columns.3
+        let xPosition = positionOfPlane.x
+        let yPosition = positionOfPlane.y
+        let zPosition = positionOfPlane.z
+        basketNode?.position = SCNVector3(xPosition, yPosition, zPosition)
+        self.sceneView.scene.rootNode.addChildNode(basketNode!)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            self.planeDetected.isHidden = false
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetected.isHidden = true
+        }
+    }
 }
 
