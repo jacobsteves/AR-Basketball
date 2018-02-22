6681e391799e11a34eb77f9ef1660f888b4bfef5
Jacob Steves <jgs1998@hotmail.com>
2018-02-21 21:48:09 -0500

    make basketball physics based

--- AR-Basketball/ViewController.swift
+++ AR-Basketball/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var planeDetected: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
-    
+    var power: Float = 1.0
+    var basketAdded: Bool {
+        return self.sceneView.scene.rootNode.childNode(withName: "Basket", recursively: false) != nil
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
@@ -24,6 +27,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let tapGestureRecongizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
         self.sceneView.addGestureRecognizer(tapGestureRecongizer)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.basketAdded == true {
+            guard let pointOfView = self.sceneView.pointOfView else {return}
+            self.power = 10
+            let transform = pointOfView.transform
+            let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+            let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+            let position = location + orientation
+            let ball = SCNNode(geometry: SCNSphere(radius: 0.3))
+            ball.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "ball")
+            ball.position = position
+            let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
+            ball.physicsBody = body
+            ball.physicsBody?.applyForce(SCNVector3(orientation.x * power, orientation.y * power, orientation.z * power), asImpulse: true)
+            self.sceneView.scene.rootNode.addChildNode(ball)
+        }
     }
     
     @objc func handleTap(sender: UITapGestureRecognizer) {
@@ -62,3 +83,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
